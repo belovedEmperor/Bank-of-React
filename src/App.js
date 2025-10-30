632bdc895047184bc09c59c85efa76959874683e
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2025-10-29 23:26:39 -0400

    Implemented AccountBalance, Home, UserProfile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      accountBalance: 9999.99,
+      currentUser: {
+        userName: "Joe Smith",
+        memberSince: "11/22/99",
+      },
+    };
+  }
+
+  render() {
+    // You no longer need to create a separate constant for components.
+    // You can pass the props directly inside the JSX for the element.
+    return (
+      <Router>
+        <Routes>
+          {/*
+              For React Router 6, you need to use <Routes> to wrap <Route> & "element" instead of "component"
+              No need for "exact" either
+          */}
+          <Route
+            path="/"
+            element={<Home accountBalance={this.state.accountBalance} />}
+          />
+          <Route
+            path="/userProfile"
+            element={
+              <UserProfile
+                userName={this.state.currentUser.userName}
+                memberSince={this.state.currentUser.memberSince}
+              />
+            }
+          />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import AccountBalance from "./AccountBalance";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://picsum.photos/800/400" alt="bank" />
+        <h1>Bank of React</h1>
+
+        <AccountBalance accountBalance={this.props.accountBalance} />
+
+        <Link to="/userProfile">User Profile</Link>
+      </div>
+    );
+  }
+}
+
+export default Home;
